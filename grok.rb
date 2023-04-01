2260773240a0d6c5cfe129cd021a07b148ef434a
Hugo Tamaki <hugo.tamaki@storyblok.com>
2023-04-01 16:20:54 -0300

    Changing mechanize instance name

--- grok.rb
+++ grok.rb
@@ -1,10 +1,10 @@
 require 'mechanize'
 require 'thread/pool'
 
-agent = Mechanize.new
-agent.get("https://www.grokpodcast.com.br")
+browser = Mechanize.new
+browser.get("https://www.grokpodcast.com.br")
 
-posts = agent.page.search(".serie-posts")
+posts = browser.page.search(".serie-posts")
 
 pool = Thread.pool(3)
 
@@ -13,12 +13,12 @@ posts.each do |post|
     links = post.search("a")
     links.each do |link|
       url = "https://www.grokpodcast.com.br#{link.attributes["href"].value}"
-      post_page = agent.get(url)
+      post_page = browser.get(url)
       download_element = post_page.search(".download-podcast").first
       folder_name = post_page.search("#serie-name").search("a").first.text
 
       puts "Saving #{download_element["download"]}..."
-      agent.get(download_element["href"]).save("grokpodcast/#{folder_name}/#{download_element["download"]}")
+      browser.get(download_element["href"]).save("grokpodcast/#{folder_name}/#{download_element["download"]}")
     end
   end
 end
